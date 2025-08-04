57cf2f38623e002d009dbba3afc5e872554d653f
jzhou <jzhou@wearemoore.com>
2025-08-04 13:37:06 -0500

    fix: add webpack alias for @ to support Vercel build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,6 +9,11 @@ const nextConfig = {
       },
     ];
   },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
+
 };
 
 module.exports = nextConfig;
